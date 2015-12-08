9285cbacf5757b4c10802392e24a115b7beefeb2
peterski74 <peterski@gmail.com>
2015-12-08 11:16:48 +1100

    added template

--- TurtlePay_Identity_Todo/App_Start/BundleConfig.cs
+++ TurtlePay_Identity_Todo/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace TurtlePay_Identity_Todo
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
+                      //"~/Content/site.css"));
         }
     }
 }
