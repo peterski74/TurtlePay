a80b69b8e481c4026c8e0f0b582598fe0022e5be
ansarjaved <ansarjaved@gmail.com>
2016-02-26 09:18:02 +1100

    changes on groups and members

--- TurtlePay_Identity_Todo/app/groups/groups.ctrl.js
+++ TurtlePay_Identity_Todo/app/groups/groups.ctrl.js
@@ -1,39 +1,91 @@
-﻿angular.module("groups", [])
+﻿
+/*
+Author Ansar Javed
+Date 19/02/2016
 
-//.filter("asDate", function () {
-//    return function (input, scope) {
-//        return new Date(input);
-//    }
-//})
+*/
+/* TodoController - the controller for the "todo view" 
+ * relies on Angular injector to provide:
+ *     $q - promises manager
+ *     $timeout - Angular equivalent of 'setTimeout'
+ *     dataservice - the application data access service
+ *     logger - the application's logging facility
+ */
 
-.controller('GroupsCtrl',
-        ['$scope', '$http',//, 'breeze', 'datacontext', '$location', '$http', '$modal'
-        function ($scope,$http) { //, breeze, datacontext, $location, $http, $modal
+//angular.module('app')
+angular.module("groups", ['ngGrid'])
 
-            $scope.test = "test x";
-            $scope.groupsList = [];
-            function getGroups(forceRefresh) {
-                $http({
-                    method: 'GET',
-                    url: 'http://localhost:65207/odata/WebAPIGroups'
-                }).success(function (_data) {
-                    getSucceeded(_data);
-                }).error(function (data, status) {
-                    console.log("Error status : " + status);
-                });
-            }
-            getGroups();
 
-            function getSucceeded(data) {
-                $scope.groupsList = data.value;
-                $scope.loading = false;
-            }
+.controller('GroupsCtrl', ['$q', '$scope', '$http', '$timeout', 'dataservice', 'logger', function ($q, $scope, $http, $timeout, dataservice, logger) {
+    // The controller's API to which the view binds
+    //var vm = this;
 
+
+
+    $scope.groupsList = [];
+    $scope.TotalItems = 0;
+
+    this.group = {};
+
+    getGroups();
+    //TotalItems();
+
+    function getGroups(forceRefresh) {
+        $http({
+            method: 'GET',
+            url: 'http://private-cff96-organisation1.apiary-mock.com/organisation'
+        }).success(function (_data) {
+            getSucceeded(_data);
+
+            logger.info("Fetched orgs");
+
+        }).error(function (data, status) {
+            console.log("Error status : " + status);
+        });
+    }
+
+
+    function getSucceeded(data) {
+        $scope.groupsList = data;
+        $scope.TotalItems = $scope.groupsList.length;
+        $scope.loading = false;
+
+
+    }
+
+
+
+    $scope.addGroup = function (group) {
+
+        alert('adding group')
+
+        $scope.groupsList.push(this.group);
+
+
+
+        ////saving to database
+        //$http.post('/odata/ProductReviews', this.review).success(function (data, status, headers) {
+
+        //});
+        ////this.Org = {};
+    };
+
+    function TotalItems() {
+        var count = $scope.groupsList.length;
+        if (count > 0) {
+            return count;
         }
-        ]);
+        return 0;
+    };
 
 
 
+
+}
+
+
+
+]);
 
 
 
--- TurtlePay_Identity_Todo/app/members/members.ctrl.js
+++ TurtlePay_Identity_Todo/app/members/members.ctrl.js
@@ -1,16 +1,94 @@
-﻿angular.module("members", [])
+﻿
+/*
+Author Ansar Javed
+Date 19/02/2016
 
-//.filter("asDate", function () {
-//    return function (input, scope) {
-//        return new Date(input);
-//    }
-//})
+*/
+/* TodoController - the controller for the "todo view" 
+ * relies on Angular injector to provide:
+ *     $q - promises manager
+ *     $timeout - Angular equivalent of 'setTimeout'
+ *     dataservice - the application data access service
+ *     logger - the application's logging facility
+ */
 
-.controller('MembersCtrl',
-        ['$scope', //, 'breeze', 'datacontext', '$location', '$http', '$modal'
-        function ($scope) { //, breeze, datacontext, $location, $http, $modal
+//angular.module('app')
+angular.module("members", [])
 
-            $scope.test = "test";
 
+.controller('MembersCtrl', ['$q', '$scope', '$http', '$timeout', 'dataservice', 'logger', function ($q, $scope, $http, $timeout, dataservice, logger) {
+    // The controller's API to which the view binds
+    //var vm = this;
+
+
+
+    $scope.MembersList = [];
+    $scope.TotalMembers = 0;
+
+    this.member = {};
+
+    getMembers();
+    //TotalItems();
+
+    function getMembers(forceRefresh) {
+        $http({
+            method: 'GET',
+            url: 'http://private-cff96-organisation1.apiary-mock.com/organisation'
+        }).success(function (_data) {
+            getSucceeded(_data);
+
+            logger.info("Fetched members");
+
+        }).error(function (data, status) {
+            console.log("Error status : " + status);
+        });
+    }
+
+
+    function getSucceeded(data) {
+        $scope.MembersList = data;
+        $scope.TotalMembers = $scope.MembersList.length;
+        $scope.loading = false;
+
+
+    }
+
+
+
+    $scope.addMember = function (member) {
+
+        alert('adding member')
+
+        $scope.MembersList.push(this.member);
+
+
+
+        ////saving to database
+        //$http.post('/odata/ProductReviews', this.review).success(function (data, status, headers) {
+
+        //});
+        ////this.Org = {};
+    };
+
+    function TotalMembers() {
+        var count = $scope.MembersList.length;
+        if (count > 0) {
+            return count;
         }
+        return 0;
+    };
+
+
+
+
+}
+
+
+
 ]);
+
+
+
+
+
+
