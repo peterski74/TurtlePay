fc14a12b75137414ac26e12938b97c1baeaf53fa
ansarjaved <ansarjaved@gmail.com>
2016-03-24 15:42:14 +1100

    implemented roles

---
+++ TurtlePay_Identity_Todo/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace TurtlePay_Identity_Todo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TurtlePay_Identity_Todo.Models.turtlepayEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TurtlePay_Identity_Todo.Models.turtlepayEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- TurtlePay_Identity_Todo/Models/AspNetUser.cs
+++ TurtlePay_Identity_Todo/Models/AspNetUser.cs
@@ -34,6 +34,7 @@ namespace TurtlePay_Identity_Todo.Models
         public bool LockoutEnabled { get; set; }
         public int AccessFailedCount { get; set; }
         public string UserName { get; set; }
+        public string Mobile { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
